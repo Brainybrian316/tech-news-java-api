59aeedd18a9dec0ea73e5dc0f8c6fe868a359c20
Brainybrian316 <brainybrian316@gmail.com>
2022-11-28 16:47:38 -0500

    hello world app, basic variable app, and practice app

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int firstVal = 4;
+        double secondVal = 19.90;
+        String myString = "This is a string";
+
+        double result = firstVal + secondVal;
+        String stringResult = myString + firstVal;
+
+        System.out.println(result);
+        System.out.println(stringResult);
+    }
+}
+
+// section create a testModel. Demo class
