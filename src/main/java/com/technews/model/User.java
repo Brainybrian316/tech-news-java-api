f0428656c9341d486ad89b4b7803fa68be924824
Brainybrian316 <brainybrian316@gmail.com>
2022-11-28 21:35:05 -0500

    Post entity model

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -10,9 +10,10 @@ import java.util.Objects;
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
-
 public class User implements Serializable {
 
+    @Transient
+    boolean loggedIn;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
@@ -20,9 +21,6 @@ public class User implements Serializable {
     @Column(unique = true)
     private String email;
     private String password;
-    @Transient
-    boolean loggedIn;
-
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Post> posts;
     // Need to use FetchType.LAZY to resolve multiple bags exception
