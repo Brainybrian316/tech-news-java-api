24a3df943794286d957c868c7d25831c979910b3
Brainybrian316 <brainybrian316@gmail.com>
2022-11-28 21:42:57 -0500

    Vote entity model

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,8 +1,7 @@
 package com.technews.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.sun.istack.NotNull;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
